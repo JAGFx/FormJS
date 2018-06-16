21306e91fc5b375ac5632414605c127b0b7891ee
JAGFx <contact@jagfx.fr>
2018-06-16 18:51:19 +0200

    chore: add gust process to minify css and js

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018.  JAGFx
+ * @author: SMITH Emmanuel
+ * @version: 2.0.0
+ */
+
+'use strict';
+
+var gulp     = require( 'gulp' );
+var sass     = require( 'gulp-sass' );
+var clean    = require( 'gulp-clean' );
+var cleanCSS = require( 'gulp-clean-css' );
+var rename   = require( 'gulp-rename' );
+var minify   = require( 'gulp-minify' );
+
+var scssPath = './scss/**/';
+var distCSS  = './dist/css/';
+
+gulp.task( 'scss:clean', function () {
+	return gulp.src( distCSS, { read: false, allowEmpty: true } )
+		.pipe( clean() );
+} );
+
+gulp.task( 'scss:build', function () {
+	return gulp.src( scssPath + '*.scss' )
+		.pipe( sass() )
+		.pipe( gulp.dest( distCSS ) );
+} );
+
+gulp.task( 'scss:watch', function () {
+	gulp.watch( scssPath + '*.scss', gulp.series( [ 'scss:clean', 'scss:build' ] ) );
+} );
+
+gulp.task( 'scss:dist', gulp.series( [ 'scss:build' ], function () {
+	console.log( distCSS + '**/*.css' );
+	return gulp.src( distCSS + '**/*.css' )
+		.pipe( cleanCSS( { compatibility: 'ie8' } ) )
+		.pipe( rename( {
+			suffix: '.min'
+		} ) )
+		.pipe( gulp.dest( distCSS ) );
+} ) );
+
+gulp.task( 'js:dist', function () {
+	return gulp.src( 'dist/*.js' )
+		.pipe( minify( {
+			ext:         {
+				src: '.js',
+				min: '.min.js'
+			},
+			ignoreFiles: [ '-min.js' ]
+		} ) )
+		.pipe( gulp.dest( 'dist' ) )
+} );
